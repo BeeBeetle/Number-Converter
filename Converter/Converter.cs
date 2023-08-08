77dc3fd2f3cc799111a5bca6e718862a1af2fa5c
BeeBeetle <131170035+BeeBeetle@users.noreply.github.com>
2023-08-08 10:28:52 -0400

    Any conversion
    Can now convert from any base to any other base.

---
+++ Converter/Converter.cs
@@ -0,0 +1,81 @@
+using System.Security.Cryptography.X509Certificates;
+using Engine;
+
+namespace Converter
+{
+    public partial class Converter : Form
+    {
+        public Converter()
+        {
+            InitializeComponent();
+        }
+        private void Converter_Load(object sender, EventArgs e)
+        {
+            //making our list of available bases
+            //because of the array in NumberChanger as it stands base 92 is the largest you can go
+            //however any base smaller than that could be added
+            var listOfBases = new List<NumberBases>();
+            listOfBases.Add(new NumberBases() { baseName = "Binary", baseValue = 2 });
+            listOfBases.Add(new NumberBases() { baseName = "Trinary", baseValue = 3 });
+            listOfBases.Add(new NumberBases() { baseName = "Septecimal", baseValue = 7 });
+            listOfBases.Add(new NumberBases() { baseName = "Decimal", baseValue = 10 });
+            listOfBases.Add(new NumberBases() { baseName = "Duodecimal", baseValue = 12 });
+            listOfBases.Add(new NumberBases() { baseName = "Hexadecimal", baseValue = 16 });
+            listOfBases.Add(new NumberBases() { baseName = "Babylonian", baseValue = 60 });
+            listOfBases.Add(new NumberBases() { baseName = "Asciidecimal", baseValue = 92 });
+            //we need to clone our original list so we can use it in two places
+            //otherwise both dropdowns will simply mirror eachother
+            var inputBases = new List<NumberBases>(listOfBases);
+            var outputBases = new List<NumberBases>(listOfBases);
+
+            //currently the input is locked to decimal
+            //this is because I haven't written a reverse conversion yet from base X into anything else
+            //other than that this defines our selections so we know what to display when someone wants to make a choice
+            //the displaymember is what the user sees
+            //the value member determines the number of the base used to convert (.SelectedValue)
+            cmbInput.DataSource = inputBases;
+            cmbInput.ValueMember = "baseValue";
+            cmbInput.DisplayMember = "baseName";
+            cmbInput.SelectedIndex = 3;
+
+            cmbOutput.DataSource = outputBases;
+            cmbOutput.ValueMember = "baseValue";
+            cmbOutput.DisplayMember = "baseName";
+            cmbOutput.SelectedIndex = 0;
+        }
+        private void rtbInput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void rtbOutput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            //here we are assigning the output text to the results of the number change method
+            //we also have to convert the input (starts as a string) into a number so we can do math
+            //the output gets to stay as a string because once we convert it doesn't matter
+            //the reverse conversion will be much harder to create because how do you do math on A%,~%*54h
+            NumberChanger numberChanger = new NumberChanger();
+            //if the two bases match (a silly case but w/e)
+            if (cmbOutput.SelectedIndex == cmbInput.SelectedIndex)
+            {
+                rtbOutput.Text = rtbInput.Text;
+            }
+            //converting from base 10 to any other base (easy)
+            else if (cmbInput.SelectedIndex == 3)
+            {
+                rtbOutput.Text = numberChanger.ConvertFrom10(Convert.ToUInt64(rtbInput.Text), Convert.ToUInt64(cmbOutput.SelectedValue));
+            }
+            //if converting from not base 10 to any other base
+            //it is important to convert to base 10 first because the math to convert to an alternate base
+            //requires that you be in base 10 because there is no math you can do outside of base 10
+            else if (cmbInput.SelectedIndex != 3)
+            {
+                string base10 = numberChanger.ConvertTo10(rtbInput.Text, Convert.ToUInt64(cmbInput.SelectedValue));
+                rtbOutput.Text = numberChanger.ConvertFrom10(Convert.ToUInt64(base10), Convert.ToUInt64(cmbOutput.SelectedValue));
+            }
+        }
+    }
+}
---
+++ UI/NumberChanger.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Engine
+{
+    public class NumberChanger
+    {
+        //this will be our final output
+        string finalNumber = "";
+        //this is our master list of characters, it gets wonky at the end
+        //but that's ok when we need to go out to base 92
+        //these are the order in which they will appear, so at base 11 the next "number" to be used will be A
+        //to change it to something else simply reorder the string below
+        char[] numbersArray = "0123456789ABCDEF†GHIJKL|MNOPQRƒS‡TuµVWXYZabcde¦þhijklmnøpqrs§ÅŠØŸ×ÂÃÄÈÉÊËÌÍ¤ÀÁÎÏŽÑÒÓÔÕÖÙÚÜÛÝ".ToCharArray();
+
+        //this is where the magic happens, we first define that we need ulongs in case someone wants to do a real big number!
+        public string ConvertFrom10(ulong toConvNumber, ulong targetBase)
+        {
+            //as long as the working number is not 0 we keep going through the loop
+            //if it ever reaches 0 that means the base has been divided for the last time
+            //any further division is useless and would only return 0
+            while (toConvNumber != 0)
+            {
+                //this sets our remainder which will determine each character in our new number
+                ulong remainder = toConvNumber % targetBase;
+                //this pulls the character from our array based on its position via the remainder
+                //the 35th character in the array is Z
+                //with a remainder of 35 Z would be the next character added to our final number 
+                //it is important that the next number is added to the front of the string because the division
+                //causes the number to be calculated in reverse, so 28 to Hex would come out as C1 instead of 1C
+                //if we added to the end of finalNumber
+                finalNumber = numbersArray[remainder] + finalNumber;
+                //this resets the working number such that the math works, I can't think of a good way to explain it
+                toConvNumber = toConvNumber / targetBase;
+            }
+            //finally we take our finished number and send it back to the rest of the program!
+            return finalNumber;
+        }
+
+        public string ConvertTo10(string inputNumber, ulong startingBase)
+        {
+            string outputString = "";
+            double outputNumber = 0;
+            char[] inputArray = inputNumber.ToCharArray();
+            int subPosition = (inputNumber.Length) - 1;
+            //Array.Reverse(inputArray);
+            foreach (char c in inputArray)
+            {
+                int masterPosition = Array.IndexOf(numbersArray, c);
+                double x = masterPosition * Math.Pow(startingBase, subPosition);
+                outputNumber += x;
+                subPosition -= 1;
+            }
+            return Convert.ToString(outputNumber);
+        }
+    }
+}
