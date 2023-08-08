0767e27b446639e457d475ab47cc19bd6bd1423f
BeeBeetle <131170035+BeeBeetle@users.noreply.github.com>
2023-08-08 13:24:21 -0400

    Updated comments on code

--- Converter/NumberChanger.cs
+++ Converter/NumberChanger.cs
@@ -15,10 +15,10 @@ namespace Engine
         //but that's ok when we need to go out to base 92
         //these are the order in which they will appear, so at base 11 the next "number" to be used will be A
         //to change it to something else simply reorder the string below
-        char[] numbersArray = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!#$%$'()*+,-./:;<=>?@[]^_`{}|~".ToCharArray();
+        char[] numbersArray = "0123456789ABCDEF†GHIJKL|MNOPQRƒS‡TuµVWXYZabcde¦þhijklmnøpqrs§ÅŠØŸ×ÂÃÄÈÉÊËÌÍ¤ÀÁÎÏŽÑÒÓÔÕÖÙÚÜÛÝ".ToCharArray();
 
         //this is where the magic happens, we first define that we need ulongs in case someone wants to do a real big number!
-        public string ConversionMethod(ulong toConvNumber, ulong targetBase)
+        public string ConvertFrom10(ulong toConvNumber, ulong targetBase)
         {
             //as long as the working number is not 0 we keep going through the loop
             //if it ever reaches 0 that means the base has been divided for the last time
@@ -39,6 +39,32 @@ namespace Engine
             }
             //finally we take our finished number and send it back to the rest of the program!
             return finalNumber;
+        }
+
+        //with the above code to convert from base 10 to another base all we need is to be able to revert back to base 10
+        //applying this followed by the above code will allow you to convert from any base to any other base
+        //because we can ALWAYS convert from 10 to anything
+        public string ConvertTo10(string inputNumber, ulong startingBase)
+        {
+            string outputString = "";
+            double outputNumber = 0;
+            char[] inputArray = inputNumber.ToCharArray();
+            //this guy ensures our exponent will be correct, because a number with 3 digits will need the exponents 2, 1, and 0
+            int subPosition = (inputNumber.Length) - 1;
+            foreach (char c in inputArray)
+            {
+                int masterPosition = Array.IndexOf(numbersArray, c);
+                //the math here is (x*(B^d))+(x*(B^d-1) ...
+                //where x is the value of the symbol you are converting to 10, B is the base you are currently in
+                //and d is the position in that number where the lowest position is 0 (just like starting from 0 w/ programming)
+                //for F1A in hex you would have (15*(16^2))+(1*(16^1))+(10*(16^0)) = 3840 + 16 + 10 = 3866
+                //at some point I could even go to decimals because the pattern continues
+                //so for x.ya it would be 16^0 + 16^-1 + 16^-2 and so on
+                double x = masterPosition * Math.Pow(startingBase, subPosition);
+                outputNumber += x;
+                subPosition -= 1;
+            }
+            return Convert.ToString(outputNumber);
         }
     }
 }
